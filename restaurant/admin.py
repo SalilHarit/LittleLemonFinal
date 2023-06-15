b5701b3c95a8dd89e231c63322fa46d6961ef393
Salil <39475669+SalilHarit@users.noreply.github.com>
2023-06-14 21:25:59 -0700

    Update admin.py

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Menu
+from .models import Menu,Booking
 
 admin.site.register(Menu)
-
+admin.site.register(Booking)
 # Register your models here.
